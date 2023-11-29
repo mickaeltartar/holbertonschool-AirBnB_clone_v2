640a54e8f7088c5b1bae20b65b07a833fac7abb4
ValentinePumpkins <quignon.val@gmail.com>
2023-11-29 21:42:15 +0100

    change init

--- models/base_model.py
+++ models/base_model.py
@@ -27,24 +27,19 @@ class BaseModel:
     # ============================================================ #
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            # Add attributs
+            self.created_at = self.updated_at = datetime.now()
             for key, value in kwargs.items():
-                if key == "updated_at" or key == "created_at":
+                if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-
-            # Managing missing attributes
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
+                if key != "__class__":
+                    setattr(self, key, value)
+                if 'name' in kwargs.keys():
+                    self.name = kwargs['name']
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     # __str__
     # ============================================================ #
@@ -58,6 +53,7 @@ class BaseModel:
     # ============================================================ #
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
