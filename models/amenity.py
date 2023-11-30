41b1b5c72597015b3580056d74560a3515ae7118
ValentinePumpkins <quignon.val@gmail.com>
2023-11-30 15:08:51 +0100

    add get env for DB or else Fs

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
 
     __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
 
-    name = Column(String(128), nullable=False)
+    else:
+        name = ""
