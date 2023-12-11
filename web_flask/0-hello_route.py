e00ca25ee10d8d77545089409c8a463b87ba0ed1
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-11 14:35:04 +0100

    add some comment in the code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-from flask import Flask
+""" start flask web app """
 
+from flask import Flask
 app = Flask(__name__)
 
 
-# Define a route for the root path "/"
+"""Define a route for the root path "/" """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the application on 0.0.0.0, port 5000
+    """ Run the application on 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
