a5d5a75b96eda050b58fda7f6a4237162ba2e654
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-11 14:52:25 +0100

    add permission & pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-
 """ start flask web app """
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Define a route for the root path "/" """
-    return 'Hello HBNB!'
+    """ Define a route for the root path """
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     """ Run the application on 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
