941292bc14d32fb56c45a48ffea75aae64ab49bc
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-11 16:14:08 +0100

    delete new line at end of the file

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,11 +20,10 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """ Returns string of a web server """
-    return "C {}".format(text.replace("_", " "))
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
     """ Run the application on 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
-
-
