8c45d1d254ab7c24081c216f9670dea1cd0b7d6a
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-13 10:17:43 +0100

    pycodestyle & code indent

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,25 +3,26 @@
 Script that starts a Flask web application and display
 a HTML page with list of states sorted by name
 """
-
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import render_template
+
 
 app = Flask(__name__)
+
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of states sorted by name"""
     states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
-def closeDB(self):
-    """ Close the database after request """
+def close_db(self):
+    """Closes the database after a request has been processed"""
     storage.close()
 
-if __name__ == "__main__":
-    """ Run the application on 0.0.0.0, port 5000 """
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
