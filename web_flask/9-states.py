e9d6fad9d1aaf54700000336b39dbcc3d5d12ec7
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-14 15:02:06 +0100

    indent

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application and display
-a HTML page with list ID of a state """
+""" starts a Flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,25 +9,25 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def state_no_ID():
-    """ define a root for the root path for states without ID """
+def states_no_id():
+    """ Defines a root for the root path for states without id """
     states = storage.all(State)
     return render_template('9-states.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def state_ID(id):
-    """ define a root for the root path for states with ID  """
+def states_id(id):
+    """ Defines a root for the root path for states with id"""
     states = storage.all(State)
-    state_ID = "State." + id if id else None
-    return render_template('9-states.html', states=states, state_ID=state_ID)
+    state_id = "State." + id if id else None
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def close_DB(self):
-    """ close DB after request """
+def close(self):
+    """ Close DB after request """
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
