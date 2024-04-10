dcbecef7db2a47951b75204add9a0173960c1ec1
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-10 13:55:10 +0200

    sort in order cites by states & display in html page & add / in root of the file 7

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,6 +15,13 @@ def states_list():
     return render_template('/7-states_list.html', states=states)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Display list of all cities by states"""
+    states = storage.all(State)
+    return render_template('/8-cities_by_states.html', states=states)
+
+
 @app.teardown_appcontext
 def close_storage(exception):
     """Closes the database after process"""
