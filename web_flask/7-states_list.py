7ffc895dbcf987c79678a16ade2844be8f1318b8
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-10 12:53:25 +0200

    add space in state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" start flask web appli """
+""" Script that starts a Flask web application """
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def state_list():
-    """ define state list """
-    states = storage.all(State)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Displays a HTML page with a list of all State objects """
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close(self):
-    """ close database after request """
+def close_session(self):
+    """ Closes the session after each request """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
