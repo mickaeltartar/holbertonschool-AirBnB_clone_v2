902533ba33712e10370c3a7ab4847760cb67962d
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-10 13:15:02 +0200

    delete space & rename some functions

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-
-from flask import Flask
-from flask import render_template
+""" Starts a Flask web application """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Displays a HTML page with a list of all State objects """
-    states = storage.all(State).values()
+    """Display a list of all State objects"""
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_session(self):
-    """ Closes the session after each request """
+def close_storage(exception):
+    """Closes the database after process"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
